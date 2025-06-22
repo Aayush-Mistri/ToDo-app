db2b85f8f020aed334a7757605f39c765116618e
system <aayushmistri810@gmail.com>
2025-06-22 14:17:20 +0530

    Final commit

--- index.js
+++ index.js
@@ -32,4 +32,7 @@ app.post('/create' , function(req , res) {
     });      
 });
 
-app.listen(3000);
+const PORT = process.env.PORT||3000;
+app.listen(PORT , ()=>{
+    console.log(`App listening on port ${PORT}`);
+})
